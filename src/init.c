e57b50ea89f91dbabea5940ea9b038c4d82e456d
joanavar <joanillo.99@gmail.com>
2024-10-07 19:49:52 +0200

    Proceso de un filosofo acabado

--- src/init.c
+++ src/init.c
@@ -18,6 +18,21 @@ static	void thinking(t_philo *philo)
 	wrtie_status(THINKING, philo);
 }
 
+void	*lone_philo(void *arg)
+{
+	t_philo *philo;
+
+	philo = (t_philo*)arg;
+	wait_all_threads(philo->table);
+	set_long(&philo->philo_mutex, &philo->last_meal_time, gettime(MILLISECOND));
+	increase_long(&philo->table->table_mutex, philo->table->threads_running_nbr);
+	wrtie_status(TAKE_FIRST_FORK, philo);
+	while (!simulation_finished(philo->table))
+		usleep(200);
+	return (NULL);
+
+}
+
 static	void eat(t_philo *philo)
 {
 	safe_mutex_handle(&philo->first_fork->fork, LOCK);
@@ -40,8 +55,10 @@ void	*dinner_simulation(void *data)
 {
 	t_philo *philo;
 
-	philo = (philo*)data;
+	philo = (t_philo*)data;
 	wait_all_threads(philo->table)//PREGUNTAR
+	set_long(&philo->philo_mutex, philo->last_meal_time, 
+		gettime(MILLISECOND));
 	increase_long(&philo->table->table_mutex, 
 		&philo->table->threads_running_nbr);
 	
@@ -65,7 +82,8 @@ void	dinner_start(t_table *table)
 	if (0 == table->nbr_limits_meals)
 		return ;
 	else if (1 == table->philo_nbr)
-		;// funcion para un solo philosofo;
+		safe_thread_handle(table->philos[0].thread.id, lone_philo, 
+			&table->philos[0], CREATE);
 	else
 	{
 		while (++i < table->philo_nbr)
--- src/monitor.c
+++ src/monitor.c
@@ -9,7 +9,7 @@ static bool philo_died(t_philo *philo)
     if (get_bool(&philo->philo_mutex, &philo->full_c))
         return (false)
     elapsed = gettime(MILLISECOND) - get_long(&philo->philo_mutex, &philo->last_meal_time);
-    t_to_die = philo->table->time_to_die//Tengo que volver a convertir en milisegundos;
+    t_to_die = philo->table->time_to_die / 1000;
     if (elapsed > t_to_die)
         return (true);
     return (false);
@@ -21,7 +21,7 @@ void    *monitor_dinner(void *data)
 {
     t_table *table;
 
-    table = (table*)data;
+    table = (t_table*)data;
     while (!all_threads_running(&table->table_mutex, table->threads_running_nbr, 
         table->philo_nbr))
         ;
